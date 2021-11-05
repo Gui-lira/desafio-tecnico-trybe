63a964706d7768e5fddf25b39ce9da45083d21b1
Gui-lira <gui.lira.martins@gmail.com>
2021-11-05 12:36:16 -0300

    dependencias feitas

---
+++ server/models/connection.js
@@ -0,0 +1,12 @@
+const { MongoClient } = require('mongodb');
+
+const URL = "mongodb://127.0.0.1:27017/";
+
+let db = null;
+
+const connection = async () => {
+    if (db) return Promise.resolve(db);
+    const conn = await MongoClient.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true,});
+    db = await conn.db('Todos');
+    return db;
+};
